1d6f4822cc6a0fcc81b4edb8eb3e90715cacfe05
Tudor <gamers118@yahoo.com>
2013-06-29 16:51:05 +0300

    added initMatrix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+const int MaxSize = 32;
+int N;
+char A[MaxSize][MaxSize];
 
+void initMatrix(char Name[]) {
+	ifstream fin(Name);
+	fin >> N;
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= N; ++j)
+			fin >> A[i][j];
+	}
+}
 
 int main() {
 	
